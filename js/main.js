9167cf7b8d233413c7ed8283aa631e71691ad4d5
Ahmed Khaled <91360901+boboos123@users.noreply.github.com>
2022-03-19 22:19:42 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let textEle = document.querySelector(".text").querySelectorAll("span");
+let daily = document.getElementById("daily")
+let weekly = document.getElementById("weekly");
+let monthly = document.getElementById("monthly");
+let stats = document.querySelectorAll(".stats");
+let weeklyScroll = document.querySelector(".weekly-scroll");
+let container = document.querySelector(".container");
+// function to add class remove to the Element
+console.log("SDa");
+document.body.addEventListener("click", function () {
+    console.log("Ssadssda");
+    
+})
+let allEle = [...textEle]
+allEle.forEach(function (e) {
+    e.addEventListener("click", function () {
+        allEle.forEach(function (ele) {
+            ele.classList.remove("active")
+        })
+        e.classList.toggle("active");
+    })
+})
+// start scrolling on daily button 
+daily.addEventListener("click", function () {
+    stats.forEach(function (e) {
+        e.scrollTo({
+            left: 0,
+            top: 0,
+            behavior:"smooth"
+        });
+    })
+})
+weekly.addEventListener("click", function () {
+    stats.forEach(function (e) {
+                e.scrollTo({
+                left: 0,
+                top: weeklyScroll.offsetTop - 40,
+                behavior: "smooth",
+                });
+})
+});
+
